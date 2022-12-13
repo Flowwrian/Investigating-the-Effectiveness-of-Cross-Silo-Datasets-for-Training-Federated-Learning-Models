8f2e596dde8a0567634dd691d574a5362eedc081
Florian Haberkorn <flo.haberkorn@gmx.de>
2022-12-13 22:56:51 +0100

    Add main.py; helper.sample_split implemented

--- scripts/helper.py
+++ scripts/helper.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 import pandas as pd
 from pandas import DataFrame
+from sklearn.model_selection import train_test_split
+import flwr
 
 
 class Dataset(Enum):
@@ -38,6 +40,32 @@ def get_samples(data: DataFrame, n: int, dataset: Dataset = Dataset.Other, max_s
 
 def _check_covid_dataset(data: DataFrame):
     return (data.location == data.location.iloc[0]).all()
+
+
+def sample_split(data: pd.Series, testing_percentage: float, x_attributes: list[str], y_attribute: str):
+    """
+    Splits sample data in x_train, x_test, y_train and y_test. Only suitable for time series data.
+    The function takes the last entry in each sample as y value.
+
+    Args:
+        data (pd.Series): Sample data.
+        testing_percentage (float): The percentage of data used for testing. Must be between 0 and 1.
+        x_attributes (list[str]): The exogene variables used for input.
+        y_attribute (str): The endogene variable (the expected model output).
+    """
+
+
+    sample_length = len(data.iloc[0].index)
+    x_data = []
+    y_data = []
+
+    for i in range(len(data)):
+        current_sample = data.iloc[i]
+        x_data.append(current_sample.iloc[range(0, sample_length - 1)][x_attributes])
+        y_data.append(current_sample.iloc[sample_length - 1][y_attribute])
+
+    #Scikit-Learn function used for convinience
+    return train_test_split(x_data, y_data, test_size=testing_percentage, random_state=0)
 
 
 #used for testing purposes
---
+++ scripts/main.py
@@ -0,0 +1,54 @@
+import math
+
+import pandas as pd
+import flwr
+
+import helper
+
+
+#Set your parameters here
+#Dataset
+DATA_PATH = "C:\\Users\\floha\\Documents\\Bachelorarbeit\\code\\datasets\\horizontal\\covid\\owid-covid-data.csv" #Change backslash to slash if not on Windows
+DATASET = helper.Dataset.Covid
+ENTRIES_PER_SAMPLE = 10
+NUMBER_OF_SAMPLES = 100
+X_ATTRIBUTES = [""]
+Y_ATTRIBUTE = ""
+PERCENTAGE_OF_TESTING_DATA = 0.2
+#Clients
+NUMBER_OF_CLIENTS = 5
+
+#Details
+ATTRIBUTES = []
+EPOCHS = 10
+VERBOSE = True
+
+if __name__ == "__main__":
+    #Preprocess data
+    data = pd.read_csv(DATA_PATH)
+    data_samples = helper.get_samples(data, ENTRIES_PER_SAMPLE, DATASET, NUMBER_OF_SAMPLES)
+    x_train, x_test, y_train, y_test = helper.sample_split(data_samples, PERCENTAGE_OF_TESTING_DATA, X_ATTRIBUTES, Y_ATTRIBUTE)
+    if VERBOSE:
+        print(f'Data loaded from {DATA_PATH} \nSplit into {str(NUMBER_OF_SAMPLES)} samples')
+
+    #define client_fn (here it's easier to access the dataset)
+    def client_fn(cid: str) -> flwr.client.NumPyClient:
+        #sample splitting logic from https://github.com/adap/flower/blob/b0bb1bb990373c35feaca9aca37c790fed029cf9/examples/simulation_tensorflow/sim.py#L48
+        partition_size = math.floor(len(data_samples) / NUMBER_OF_CLIENTS)
+        idx_from, idx_to = int(cid) * partition_size, (int(cid) + 1) * partition_size
+
+        x_train_cid = x_train[idx_from:idx_to]
+        y_train_cid = y_train[idx_from:idx_to]
+
+        x_test_cid = x_test[idx_from:idx_to]
+        y_test_cid = y_test[idx_from:idx_to]
+
+        #TODO return client
+        pass
+
+
+    #start simulation
+    hist = flwr.simulation.start_simulation(
+        client_fn=client_fn,
+        num_clients=NUMBER_OF_CLIENTS
+    )
