79669452d5f35b7355050a06677ee1426351f436
Flowwrian <flo.haberkorn@gmx.de>
2023-02-06 22:22:29 +0100

    Ran run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -7,11 +7,11 @@ cd scripts/
 declare -a datasets=("covid" "weather")
 declare -a clients=(2 4)
 declare -a rounds=(2 4)
-declare -a sk_models=("linear regression", "linearSVR")
-declare -a tf_models=("MLP", "LSTM", "CNN")
+declare -a sk_models=("linear regression" "linearSVR")
+declare -a tf_models=("MLP" "LSTM" "CNN")
 
 declare -a stations=("berlin_alexanderplatz" "frankfurt_am_main_westend" "hamburg_airport" "leipzig" "muenchen" "potsdam")
-declare -a covid_features=("new_cases", "weekly_hosp_admissions", "new_deaths", "weekly_icu_admissions")
+declare -a covid_features=("new_cases" "weekly_hosp_admissions" "new_deaths" "weekly_icu_admissions")
 
 declare -a epochs=(2 10)
 declare -a hidden_layers=(2 3 4)
@@ -27,7 +27,7 @@ do
         #sklearn models
         for m in "${sk_models[@]}"
         do
-            python main.py --m "$m" --d weather --attributes temp rhum dwpt --r "$r" --c "$c" --sa 10000 --l MAE --batch_size 32 --standardize True --hidden_layers 5 --stations "${stations[@]:0:$c}" --log True
+            python main.py --m "$m" --d weather --attributes temp rhum dwpt --r "$r" --c "$c" --sa 10000 --l MAE --batch_size 32 --standardize True --hidden_layers 5 --stations ${stations[@]:0:$c} --log True
         done
 
         #tf models
@@ -39,7 +39,7 @@ do
                 do
                     for b in "${weather_batch_size[@]}"
                     do
-                        python main.py --m "$m" --d weather --attributes temp rhum dwpt --r "$r" --c "$c" --sa 10000 --l MAE --batch_size 32 --standardize True --stations "${stations[@]:0:$c}" --epochs "$e" --hidden_layers "$h" --batch_size "$b" --log True
+                        python main.py --m "$m" --d weather --attributes temp rhum dwpt --r "$r" --c "$c" --sa 10000 --l MAE --batch_size 32 --standardize True --stations ${stations[@]:0:$c} --epochs "$e" --hidden_layers "$h" --batch_size "$b" --log True
                     done
                 done
             done
